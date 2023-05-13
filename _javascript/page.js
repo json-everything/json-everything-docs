4d2697ae63f739ef3197aa4eb113eb0594e3d9c4
Greg Dennis <gregsdennis@yahoo.com>
2023-05-12 20:45:30 -0700

    save doclist state

--- _javascript/page.js
+++ _javascript/page.js
@@ -1,9 +1,10 @@
 import { basic, initSidebar, initTopbar } from './modules/layouts';
-import { imgExtra, initClipboard, smoothScroll } from './modules/plugins';
+import { imgExtra, initClipboard, smoothScroll, toc } from './modules/plugins';
 
 basic();
 initSidebar();
 initTopbar();
 imgExtra();
 initClipboard();
+toc();
 smoothScroll();
